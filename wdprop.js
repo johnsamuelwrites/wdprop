ca8790a7f01bb11d8a5b68f83cd6199c5ef6596b
John Samuel <johnsamuelwrites@gmail.com>
2018-05-22 15:02:01 +0200

    Add provenance information

--- wdprop.js
+++ wdprop.js
@@ -1020,6 +1020,21 @@ function getProperties() {
   queryWikidata(sparqlQuery, createDivPropertyDetails, "allProperties");
 }
 
+function getOverallProvenance() {
+  const sparqlQuery = `PREFIX wikibase: <http://wikiba.se/ontology#>
+   SELECT DISTINCT ?property 
+    {
+      ?property a wikibase:Property;
+         ?prop ?statement.
+      OPTIONAL{?statement prov:wasDerivedFrom ?reference}
+      OPTIONAL{?property wdt:P1628 ?equivproperty}
+      FILTER(REGEX(STR(?statement), "http://www.wikidata.org/entity/statement/") && bound(?reference))
+    }
+    ORDER by ?property
+    `;
+  queryWikidata(sparqlQuery, createDivPropertyDetails, "overallprovenance");
+}
+
 function getPropertiesNeedingTranslation() {
   getPropertyLabelsNeedingTranslation();
   getPropertyDescriptionsNeedingTranslation();
@@ -1070,6 +1085,20 @@ function getPropertyDetails() {
   link = document.createElement("a"); 
   link.setAttribute('href', "path.html?property=" + property)
   var text = document.createTextNode("path.html?property="+property);
+  link.appendChild(text);
+  td.appendChild(link)
+  tr.appendChild(td);
+  table.appendChild(tr);
+  div.appendChild(table);
+
+  tr = document.createElement("tr"); 
+  td = document.createElement("td"); 
+  td.innerHTML = "Provenance information";
+  tr.appendChild(td);
+  td = document.createElement("td"); 
+  link = document.createElement("a"); 
+  link.setAttribute('href', "propertyprovenance.html?property=" + property)
+  var text = document.createTextNode("provenance.html?property="+property);
   link.appendChild(text);
   td.appendChild(link)
   tr.appendChild(td);
@@ -1809,4 +1838,121 @@ function getPath() {
     order by ?time
     `;
   queryWikidata(sparqlQuery, createDivTranslationPath, "translationPath");
+}
+
+function getReferences() {
+  var item = "P31";
+  if(window.location.search.length > 0) {
+    var reg = new RegExp("property=([^&#=]*)");
+    var value = reg.exec(window.location.search);
+    if (value != null) {
+       item = decodeURIComponent(value[1]);
+    }
+  }
+  var div = document.getElementById("itemCode");
+  div.innerHTML = item;
+
+  const sparqlQuery = `
+    SELECT ?statement ?prop ?reference
+    {
+      wd:` + item + ` ?prop ?statement.
+      OPTIONAL{?statement prov:wasDerivedFrom ?reference}
+      FILTER(REGEX(STR(?statement), "http://www.wikidata.org/entity/statement/"))
+    }
+    ORDER by ?statement
+    `;
+  queryWikidata(sparqlQuery, createDivReferences, "references");
+}
+
+function createDivReferences(divId, json) {
+  const { head: { vars }, results } = json;
+  var references = document.getElementById(divId);
+  refs = {};
+  for ( const result of results.bindings ) { 
+    if (result["reference"] != undefined) {
+      if (result['prop'].value in refs){
+        refs[result['prop'].value] +=1;
+      }
+      else {
+        refs[result['prop'].value] =1;
+      }
+    }
+  }
+  var statementTotal = document.createElement("h3");
+  statementTotal.innerHTML = "Total " + Object.keys(refs).length + " reference statements" +
+       " for a total of " + results.bindings.length + " statements";
+  references.appendChild(statementTotal);
+
+  if(Object.keys(refs).length == 0) {
+    return;
+  }
+
+  var table = document.createElement("table"); 
+  var th = document.createElement("tr"); 
+  var td = document.createElement("th"); 
+  td.innerHTML = "Property";
+  th.appendChild(td);
+  td = document.createElement("th"); 
+  td.innerHTML = "Number of statements";
+  th.appendChild(td);
+  table.append(th);
+  console.log(refs);
+  console.log(Object.keys(refs));
+  data = Object.keys(refs);
+  for ( i=0; i<data.length; i++) {
+    console.log(data[i]);
+    tr = document.createElement("tr");
+
+    td = document.createElement("td"); 
+    td.setAttribute('class', "property");
+    var a = document.createElement("a"); 
+    a.setAttribute('href', data[i]);
+    var text = document.createTextNode(data[i].replace("http://www.wikidata.org/prop/",""));
+    a.append(text);
+    td.appendChild(a);
+    tr.appendChild(td);
+  
+    td = document.createElement("td"); 
+    text = null;
+    text = document.createTextNode(refs[data[i]]);
+    td.appendChild(text);
+    tr.appendChild(td);
+    table.appendChild(tr);
+    
+  }
+  references.appendChild(table);
+}
+
+function getEquivalentProperties() {
+  var item = "P31";
+  if(window.location.search.length > 0) {
+    var reg = new RegExp("property=([^&#=]*)");
+    var value = reg.exec(window.location.search);
+    if (value != null) {
+       item = decodeURIComponent(value[1]);
+    }
+  }
+
+  const sparqlQuery = `
+    SELECT ?equivproperty
+    {
+      wd:`+ item +` wdt:P1628 ?equivproperty
+    }
+
+    `;
+  queryWikidata(sparqlQuery, createDivExternalLinks, "externalEquivProperties");
+}
+
+function createDivExternalLinks(divId, json) {
+  const { head: { vars }, results } = json;
+  var references = document.getElementById(divId);
+  refs = {};
+  var statementTotal = document.createElement("h3");
+  statementTotal.innerHTML = "Total " + results.bindings.length + " equivalent properties on external sources";
+  references.appendChild(statementTotal);
+}
+
+function getLinks() {
+  getReferences();
+  getEquivalentProperties();
 }
